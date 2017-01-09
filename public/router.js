bfc5222148092e0d2dff73e3dd6c34c685cc7238
Kyle Edwards <clydesbro@gmail.com>
2017-01-09 13:09:49 -0600

    Updated

--- public/router.js
+++ public/router.js
@@ -21,7 +21,8 @@ if (window.FC === undefined) {
       React.createElement(Route, { path: "/create-set", component: FC.SetEditorComponent }),
       React.createElement(Route, { path: "/set/:setId", component: FC.SetViewComponent }),
       React.createElement(Route, { path: "/set/:setId/newcard", component: FC.CardEditorComponent }),
-      React.createElement(Route, { path: "/set/:setId/quizzer", component: FC.QuizzerComponent })
+      React.createElement(Route, { path: "/set/:setId/quizzer", component: FC.QuizzerComponent }),
+      React.createElement(Route, { path: "/summary" })
     )
   );
 
