646c7f8b8816debbfd2b5d5f3144a94491fd7366
Kyle Edwards <clydesbro@gmail.com>
2017-01-09 13:09:30 -0600

    Updated

---
+++ client/js/components/SummaryComponent.js
@@ -0,0 +1,13 @@
+if (window.FC === undefined) {window.FC = {}; }
+
+(function() {
+  class Summary extends React.Component {
+    render() {
+      return(
+        <div>
+          summary
+        </div>
+      )
+    }
+  }
+}());
