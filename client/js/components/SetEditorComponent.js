035b99f384f02fd2518b149b66eee45dc95da328
David Kurth <david-kurth@sbcglobal.net>
2017-01-10 15:57:32 -0600

    set editor validation is done

--- client/js/components/SetEditorComponent.js
+++ client/js/components/SetEditorComponent.js
@@ -4,33 +4,82 @@ if (window.FC === undefined) { window.FC = {}; }
 
   class SetEditorComponent extends React.Component {
 
+    constructor() {
+      super();
+      this.state = {}
+    }
+
     submitSet(evt) {
       evt.preventDefault();
+      var makeAjaxCall = true;
 
-      $.ajax({
-        url: '/api/sets',
-        method: 'POST',
-        data: {
-          name: this.nameInput.value,
-          description: this.descriptionTextarea.value
-        }
-      })
-      .done((data) => {
-        ReactRouter.browserHistory.goBack();
-      });
+      if (this.nameInput.value === '') {
+        makeAjaxCall = false;
+        console.log("met 1");
+        this.setState({
+          nameInvalid: true
+        });
+      }
+      else {
+        console.log("met 1 else");
+        this.setState({
+          nameInvalid: false
+        });
+      }
+
+      if (this.descriptionTextarea.value === '') {
+        makeAjaxCall = false;
+        console.log('met 2');
+        this.setState({
+          textAreaInvalid: true
+        });
+      }
+      else {
+        console.log('met 2 else');
+        this.setState({
+          textAreaInvalid: false
+        });
+      }
+
+      if (makeAjaxCall) {
+        $.ajax({
+          url: '/api/sets',
+          method: 'POST',
+          data: {
+            name: this.nameInput.value,
+            description: this.descriptionTextarea.value
+          }
+        })
+        .done((data) => {
+          ReactRouter.browserHistory.goBack();
+        });
+      }
 
 
     }
 
     render() {
+      console.log(this.state);
+      var nameInputClass = '';
+      if (this.state.nameInvalid) {
+        console.log('name is invalid');
+        nameInputClass = 'invalid';
+      }
+
+      var descriptionTextareaClass = '';
+      if (this.state.textAreaInvalid) {
+        descriptionTextareaClass = 'invalid';
+
+      }
+
       return <div className="set-editor">
         <h2>Set Editor</h2>
 
         <form onSubmit={(evt) => { this.submitSet(evt); }}>
 
-          <input placeholder="name" ref={(input) => { this.nameInput = input; }} />
+          <input className={nameInputClass} placeholder="name" ref={(input) => { this.nameInput = input; }} />
 
-          <textarea placeholder="description" ref={(textarea) => { this.descriptionTextarea = textarea; }} />
+          <textarea className={descriptionTextareaClass} placeholder="description" ref={(textarea) => { this.descriptionTextarea = textarea; }} />
 
           <button>Save</button>
         </form>
